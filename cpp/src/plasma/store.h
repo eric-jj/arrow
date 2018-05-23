7d419d2a6db138ccb19f13e41c482d63bceb46c2
Zhijun Fu <pingfu.fzj@antfin.com>
2018-05-23 21:39:07 +0800

    Plasma queue create & push support, a prototyped simple buffer manager, commentted Jin's original buffer management code to make code compile

--- cpp/src/plasma/store.h
+++ cpp/src/plasma/store.h
@@ -24,6 +24,7 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#include <queue>
 
 #include "plasma/common.h"
 #include "plasma/events.h"
@@ -50,6 +51,20 @@ struct Client {
 
   /// Object ids that are used by this client.
   std::unordered_set<ObjectID> object_ids;
+};
+
+struct SimpleQueueItemRecord {
+  SimpleQueueItemRecord() : seq_id(0), data_offset(0), data_size(0) {}
+  uint64_t seq_id;
+  uint64_t data_offset;
+  uint32_t data_size;
+};
+
+struct SimpleQueueData {
+  uint8_t* pointer;
+  uint64_t size;
+
+  std::queue<SimpleQueueItemRecord> queue;
 };
 
 static const int32_t QUEUE_BLOCK_SIZE = 1000; 
@@ -111,6 +126,8 @@ class PlasmaStore {
                                      QueueBlockHeader* block_header, int32_t offset);
 
   int push_queue(const ObjectID& object_id, uint8_t* data, int64_t data_size);
+
+  int create_queue_item(const ObjectID& object_id, int64_t data_size, SimpleQueueItemRecord* new_record);
 
   /// Abort a created but unsealed object. If the client is not the
   /// creator, then the abort will fail.
@@ -225,6 +242,8 @@ class PlasmaStore {
 #ifdef PLASMA_GPU
   arrow::gpu::CudaDeviceManager* manager_;
 #endif
+
+  std::unordered_map<ObjectID, std::unique_ptr<SimpleQueueData>> queues_;
 };
 
 }  // namespace plasma
